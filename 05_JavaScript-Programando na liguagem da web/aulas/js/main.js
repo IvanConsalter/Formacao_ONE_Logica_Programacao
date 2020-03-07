489c1e953ea128928c30cf53b9b6a4a9cdd2c3d0
IvanConsalter <ivanconsalter1@gmail.com>
2020-03-07 14:49:06 -0300

    addEventListener(), createElement(), value, appendChild()

--- 05_JavaScript-Programando na liguagem da web/aulas/js/main.js
+++ 05_JavaScript-Programando na liguagem da web/aulas/js/main.js
@@ -36,3 +36,42 @@ for(var i = 0; i < pacientes.length; i++) {
 		tdImc.textContent = 'Altura e/ou peso inválidos!'
 	}	
 }
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+botaoAdicionar.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+   
+   var form = document.querySelector('#form-adiciona');
+
+   var nome = form.nome.value;
+   var peso = form.peso.value;
+   var altura = form.altura.value;
+   var gordura = form.gordura.value;
+
+   var trPaciente = document.createElement('tr');
+
+   var tdNome = document.createElement('td');
+   var tdPeso = document.createElement('td');
+   var tdAltura = document.createElement('td');
+   var tdGordura = document.createElement('td');
+   var tdImc = document.createElement('td');
+
+   tdNome.textContent = nome;
+   tdPeso.textContent = peso;
+   tdAltura.textContent = altura;
+   tdGordura.textContent = gordura;
+
+   trPaciente.appendChild(tdNome);
+   trPaciente.appendChild(tdPeso);
+   trPaciente.appendChild(tdAltura);
+   trPaciente.appendChild(tdGordura);
+
+   var tabela = document.querySelector('#tabela-pacientes');
+
+   tabela.appendChild(trPaciente);
+});
+
+
+
+
